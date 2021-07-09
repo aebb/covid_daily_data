81c643d52e833898f8e3ddf255d88a3f7b35ae16
aebb <20627367+aebb@users.noreply.github.com>
2021-07-09 22:43:52 +0100

    graph colors

--- config.r
+++ config.r
@@ -2,7 +2,8 @@ url <- 'https://raw.githubusercontent.com/dssg-pt/covid19pt-data/master/data.csv
 last_n_days <- 30
 source <- 'https://github.com/dssg-pt/covid19pt-data'
 image_file <- 'image.png'
-line_color <- '#f95d6a'
-point_color <- '#f95d6a'
+image_file2 <- 'image2.png'
+line_color <- '#198ce7'
+point_color <- '#198ce7'
+text_color <- '#a0a0a3'
 commit_message_file <- 'commit_message.txt'
-remote_image_url <- 'https://raw.githubusercontent.com/aebb/covid_daily_data/master/image.png'
--- script.r
+++ script.r
@@ -1,32 +1,36 @@
 library(ggplot2)
+library(ggthemes)
 source('config.r')
 
 temporary_file = 'data.csv'
 
 download.file(url = url, destfile = temporary_file, method = 'curl')
 
-data_set <- tail(read.csv(file = temporary_file )[c('data','confirmados_novos')], last_n_days)
 
+#Evolution graph
+data_set <- tail(read.csv(file = temporary_file )[c('data','confirmados_novos')], last_n_days)
 ggplot(data_set, aes(x = as.Date(factor(data, ordered = T), format = "%d-%m-%Y"), y = confirmados_novos, group = 1)) +
     geom_line(color = line_color) +
     geom_point(color = point_color) +
     scale_x_date(expand = c(0.02, 0), date_labels = "%d %b", date_breaks = "1 day") +
-    geom_text(aes(label = confirmados_novos), vjust = -1, size = 2.2) +
+    geom_text(aes(label = confirmados_novos), vjust = -1, size = 2.2, color = text_color) +
     labs(
-        title= sprintf('Number of new covid19 cases in Portugal, last %i days', last_n_days), 
+        title= sprintf('Covid19 cases in Portugal, last %i days', last_n_days), 
         caption=sprintf('Source: %s', source), 
         x='Date',
-        y='Number of cases', 
+        y='Cases', 
         color=NULL
     ) + 
+    theme_hc(style = "darkunica") +
+    scale_colour_hc("darkunica") +
     theme(
-        panel.grid.minor = element_blank(),
         axis.text.y = element_text(size = 10, hjust = 1),
         axis.text.x = element_text(size = 10, angle = 45, hjust = 1),
+        plot.title = element_text(hjust = 0.5), 
+        panel.grid.minor = element_blank(),
     )
-
 ggsave(image_file, dpi = 300)
 
 today_number_of_cases <- tail(data_set['confirmados_novos'], n = 1)
 date <- tail(data_set['data'], n = 1)
-cat(sprintf("%s new cases on %s %s", today_number_of_cases, date, remote_image_url), file = commit_message_file)
+cat(sprintf("%s new cases on %s", today_number_of_cases, date), file = commit_message_file)
